4ea6b26e85daef42bf21be60a516ea7e2bb70702
bpoche <bpoche@vt.edu>
2017-12-09 02:05:45 -0500

    added err_compare function

--- err_maps.py
+++ err_maps.py
@@ -64,9 +64,7 @@ def err_maps(img=['mx3'],
     import itertools
     import numpy as np
     from scipy.interpolate import griddata
-    from gen_stars import gen_stars
     from star_utils import img_stars,load_stars,rebin,trim
-    data = gen_stars()
     
     data = load_stars()
     x_act=[]
@@ -175,7 +173,109 @@ def err_maps(img=['mx3'],
     
     return err_maps
 
-
+def err_compare(img,err_maps,plot_dmap=False):
+    '''
+    Apply an error map to the centroid coordinates of a given list of images
+    and return statistics of the error. Option to plot distortion map.
+    
+    inputs:
+        img: list of image to test error map on ie ['mx2']
+        err_map: error maps to apply to image
+        dmap: set to True to plot resulting distortion map
+        
+    Outputs:
+        err_stats: statistics on error between modeled star pixel locations and
+                   the centroid pixel locations that were 'corrected' by error map
+                   
+    Example Usage:
+        1) create error map for 'mx' camera using images 'mx2' and 'mx3' and then
+            apply error map to image 'mx1' and see how well the error is corrected
+            
+            import pandas as pd
+            from err_maps import err_maps,err_compare
+            err_maps = err_maps(['mx1','mx2'],fit='2d polyfit',order=2)
+            err_compare(['mx3'],err_maps,plot_dmap=True)
+    '''
+    from star_utils import img_stars,load_stars
+    from dmap import dmap
+    import numpy as np
+    import pandas as pd
+    
+    data=load_stars()
+    x_act=[]
+    y_act=[]
+    x_img=[]
+    y_img=[]
+    for i in range(len(img)):
+        col_list = ['x_act_'+img[i],
+                    'y_act_'+img[i],
+                    'x_img_'+img[i],
+                    'y_img_'+img[i]]
+        df = img_stars(data,img[i],col_list)
+        x_act.append(list(df['x_act_'+img[i]]))
+        y_act.append(list(df['y_act_'+img[i]]))
+        x_img.append(list(df['x_img_'+img[i]]))
+        y_img.append(list(df['y_img_'+img[i]]))
+    x_act = np.array([item for sublist in x_act for item in sublist])
+    y_act = np.array([item for sublist in y_act for item in sublist])
+    x_img = np.array([item for sublist in x_img for item in sublist])
+    y_img = np.array([item for sublist in y_img for item in sublist])
+    
+    x_img_new=[]
+    y_img_new=[]
+    for i in range(len(x_img)):
+        x_img_new.append(x_img[i] - err_maps['offx_1520'][int(x_img[i]),int(y_img[i])])
+        y_img_new.append(y_img[i] - err_maps['offy_1520'][int(x_img[i]),int(y_img[i])])
+    x_img_new=np.array(x_img_new)
+    y_img_new=np.array(y_img_new)
+    
+    x_err_old = x_img - x_act
+    y_err_old = y_img - y_act
+    mag_err_old = np.sqrt(x_err_old**2+y_err_old**2)
+    
+    x_err_new = x_img_new - x_act
+    y_err_new = y_img_new - y_act
+    mag_err_new = np.sqrt(x_err_new**2+y_err_new**2)
+    
+    err_stats = {'x_err_old':x_err_old,
+                 'y_err_old':y_err_old,
+                 'mag_err_old':mag_err_old,
+                 'x_err_new':x_err_new,
+                 'y_err_new':y_err_new,
+                 'mag_err_new':mag_err_new}
+    
+    if plot_dmap == True:
+        data_dmap = {'x_img_'+img[0]:x_img,
+                    'y_img_'+img[0]:y_img,
+                    'x_act_'+img[0]:x_act,
+                    'y_act_'+img[0]:y_act,
+                    'x_img_new_'+img[0]:x_img_new,
+                    'y_img_new_'+img[0]:y_img_new,
+                    'mag_err_old_'+img[0]:mag_err_old,
+                    'mag_err_new_'+img[0]:mag_err_new}
+        err_data = pd.DataFrame(data_dmap)
+        
+        dmap(err_data,
+             x_act='x_act_',
+             y_act='y_act_',
+             x_img='x_img_',
+             y_img='y_img_',
+             err_mag='mag_err_old_',
+             title='Original Distortion Map',
+             img_list=img)
+        dmap(err_data,
+             x_act='x_act_',
+             y_act='y_act_',
+             x_img='x_img_new_',
+             y_img='y_img_new_',
+             err_mag='mag_err_new_',
+             title='Distortion Map after error correction',
+             img_list=img)
+    
+    return err_stats
+    
+    
+    
     
     
     
