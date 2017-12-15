aee1c3b1cc750563cb5c824939113b5c05e54c3d
MuhammadVT <mamatripik@gmail.com>
2017-12-15 09:15:26 -0500

    add a new version of dmap_compare

--- custom_functions.py
+++ custom_functions.py
@@ -109,5 +109,49 @@ def pol2cart(phi, rho):
     import numpy as np
     x = np.multiply(rho, np.cos(phi)) 
     y = np.multiply(rho, np.sin(phi))
+
     return x, y
+
+def dmap_compare_v2(d_old, d_new,
+		    suptitle='Distortion Map Comparison',
+		    title1='Original Distortion Map',
+		    title2='Distortion Map after error correction'):
+
+    from dmap import dmap
+    from matplotlib.colors import Normalize
+    import matplotlib.cm as cm
+    import matplotlib.pyplot as plt
+
+    dmap_old = dmap(d_old,show_plot=False)
+    dmap_new = dmap(d_new,show_plot=False)
+
+    norm = Normalize()
+    norm.autoscale(dmap_old['v_colors'])
+    colormap = cm.jet
+    sm = cm.ScalarMappable(cmap=colormap, norm=norm)
+    sm.set_array([])
+    fig, (ax1, ax2) = plt.subplots(ncols=2,figsize=(14, 5), facecolor='w', edgecolor='k')
+    fig.set_tight_layout(False)
+
+    ax1=plt.subplot(121)
+    dm=dmap_old
+    plt.title(title1)
+    plt.quiver(dm['v_err_loc_x'],dm['v_err_loc_y'],
+              dm['v_err_x'],dm['v_err_y'],
+              color=colormap(norm(dm['v_colors'])))
+    fig.colorbar(sm)
+
+    ax2=plt.subplot(122)
+    dm=dmap_new
+    plt.title(title2)
+    plt.quiver(dm['v_err_loc_x'],dm['v_err_loc_y'],
+              dm['v_err_x'],dm['v_err_y'],
+              color=colormap(norm(dm['v_colors'])))
+    fig.colorbar(sm)
+
+    plt.suptitle(suptitle,fontsize=16)
+
+    return
+
+
 
