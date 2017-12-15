87d90c21bf5d78dd19c313352dc663cea2629063
bpoche <bpoche@vt.edu>
2017-12-15 20:54:30 -0100

    added refdoc/example usage

--- dmap_compare.py
+++ dmap_compare.py
@@ -11,6 +11,51 @@ def dmap_compare(d_old_train,
                  suptitle='Distortion Map Comparison',
                  title1='Original Distortion Map',
                  title2='Distortion Map after error correction'):
+    '''
+    Plot distortion maps before and after error correction
+    Input:
+        d_old_train: dict of distortion parameters for original training dataset
+                    x_img: numpy array of x-pixel centroid locations
+                    y_img: numpy array of y-pixel centroid locations
+                    x_act: numpy array of x-pixel modeled star locations
+                    y_act: numpy array of y-pixel modeled locations
+                    xdiff: numpy array of x_img-x_act
+                    ydiff: numpy array of y_img-y_act
+                    mag: numpy array of magnitude of error (np.sqrt(xdiff**2+ydiff**2))
+                    img: list of images used to produce above data ie (['mx1','mx2'])
+                    rows: number of rows (y-pixel coordinates) in images (default = 1520)
+                    cols: number of cols (x-pixel coordinates) in images (default = 1520)
+        d_old_test: distortion parameters for original testing dataset
+        d_new_train: distortion parameters for training dataset after error correction
+        d_new_test: distortion parameters for testing dataset after error correction
+        suptitle: Title of figure
+        title1: Title of subplot 1: distortion map before error correction
+        title2: Title of subplot 2: distortion map after error correction
+            
+            
+    Output:
+        Plot of distortion maps before and after error correction
+        
+    Example Usage:
+        1)Plot distortion map using 'mx2' as a training set and 'mx3' as a test set
+            from gen_d_params import gen_d_params   
+            from err_maps import err_maps
+            from dmap_compare import dmap_compare
+    
+            train_set=['mx2']
+            test_set=['mx3']
+
+            d_orig_train = gen_d_params(train_set)
+            d_orig_test = gen_d_params(test_set)
+            
+            fit=['nearest']
+            order=[None]
+            e_maps_nearest,d_new_train_nearest,d_new_test_nearest = err_maps(d_orig_train,d_orig_test,fit[0],order[0])
+            
+            dmap_compare(d_orig_train,d_orig_test,d_new_train_nearest,d_new_test_nearest,
+            suptitle='Nearest Neighbor')
+            
+    '''
     
     from dmap import dmap
     from matplotlib.colors import Normalize
@@ -44,6 +89,6 @@ def dmap_compare(d_old_train,
               color=colormap(norm(dm['v_colors'])))
     fig.colorbar(sm)
 
-    plt.suptitle(suptitle,fontsize=16)
+    plt.suptitle(suptitle,fontsize=20)
     
     return 
